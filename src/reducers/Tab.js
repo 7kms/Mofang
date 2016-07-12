4c55868aea6aad7d2643a6e3e64f6abdbffc361c
tangliang <tangliang@mofanghr.com>
2016-07-12 11:20:54 +0800

    confing eslint

--- src/reducers/Tab.js
+++ src/reducers/Tab.js
@@ -4,11 +4,10 @@ const tabInitialState = {
     selectedTab: 'interview-online'
 };
 export function Tab(state = tabInitialState, action = {}){
+  const {tab} = action;
   switch (action.type) {
     case types.TAB_CHANGE:
-      const {tab} = action;
       return {...state,...tab};
-      break;
     default:
       return state;
   }
